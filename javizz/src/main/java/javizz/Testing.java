2a7462eda0a86261fd2270384795ca91fdc6dd50
Victor Gambier <banon.victor@gmail.com>
2019-09-05 16:53:02 +0200

    Added real-time file system monitoring

--- javizz/src/main/java/javizz/Testing.java
+++ javizz/src/main/java/javizz/Testing.java
@@ -7,9 +7,16 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.vfs2.FileChangeEvent;
+import org.apache.commons.vfs2.FileListener;
+import org.apache.commons.vfs2.FileSystemManager;
+import org.apache.commons.vfs2.VFS;
+import org.apache.commons.vfs2.impl.DefaultFileMonitor;
 import org.openflexo.foundation.resource.FileSystemBasedResourceCenter;
 import org.openflexo.pamela.annotations.ImplementationClass;
 import org.openflexo.pamela.factory.DeserializationPolicy;
@@ -101,173 +108,221 @@ public class Testing {
 
 		// TODO: Initializing a watch service to track file changes on disk
 
-		// Reading a test folder
-		String folderPath = "testFiles"; // a relative path, pointing to the testFiles directory included in the project
-		ProjectLink projectLink = new ProjectLink(folderPath);
+		new Thread(new Runnable() {
+			public void run() {
 
-		// Testing to see if the data was properly gathered
+				Executor runner = Executors.newFixedThreadPool(1);
+				runner.execute(new Runnable() {
 
-		System.out.println("Here is an overview of the models:");
-		ProjectModel projectModel = projectLink.getProjectModel();
-		List<PackageModel> packages = projectModel.getPackages();
+					@Override
+					public void run() {
+						org.apache.commons.vfs2.FileObject listendir = null;
+						try {
+							FileSystemManager fsManager = VFS.getManager();
+							String absolutePath = "/homes/v17gambi/git/javizz/javizz/testFiles/firstPackage/HelloWorld.java";
+							listendir = fsManager.resolveFile(absolutePath);
+						} catch (org.apache.commons.vfs2.FileSystemException e) {
+							e.printStackTrace();
+						}
+						DefaultFileMonitor fm = new DefaultFileMonitor(new FileListener() {
 
-		for (PackageModel packageModel : packages) {
-			System.out.println("\tpackage: " + packageModel.getName());
+							@Override
+							public void fileDeleted(FileChangeEvent event) throws Exception {
+								System.out.println(event.getFile().getName().getPath() + " Deleted.");
+							}
 
-			List<ClassModel> classes = packageModel.getClasses();
-			for (ClassModel classModel : classes) {
-				System.out.println("\t\tclass: " + classModel.getName());
+							@Override
+							public void fileCreated(FileChangeEvent event) throws Exception {
+								System.out.println(event.getFile().getName().getPath() + " Created.");
+							}
 
-				List<AttributeModel> attributes = classModel.getAttributes();
-				for (AttributeModel attributeModel : attributes) {
-					System.out.println("\t\t\tattribute: " + attributeModel.getName());
+							@Override
+							public void fileChanged(FileChangeEvent event) throws Exception {
+								System.out.println(event.getFile().getName().getPath() + " Changed.");
+							}
+						});
+
+						fm.setRecursive(true);
+						fm.addFile(listendir);
+						fm.start();
+
+					}
+				});
+
+			}
+		}).start();
+
+		// In a separate thread, run the demonstration
+		new Thread(new Runnable() {
+			public void run() {
+				try {
+
+					// Reading a test folder
+					String folderPath = "testFiles"; // a relative path, pointing to the testFiles directory included in the project
+					ProjectLink projectLink = new ProjectLink(folderPath);
+
+					// Testing to see if the data was properly gathered
+
+					System.out.println("Here is an overview of the models:");
+					ProjectModel projectModel = projectLink.getProjectModel();
+					List<PackageModel> packages = projectModel.getPackages();
+
+					for (PackageModel packageModel : packages) {
+						System.out.println("\tpackage: " + packageModel.getName());
+
+						List<ClassModel> classes = packageModel.getClasses();
+						for (ClassModel classModel : classes) {
+							System.out.println("\t\tclass: " + classModel.getName());
+
+							List<AttributeModel> attributes = classModel.getAttributes();
+							for (AttributeModel attributeModel : attributes) {
+								System.out.println("\t\t\tattribute: " + attributeModel.getName());
+							}
+
+							List<MethodModel> methods = classModel.getMethods();
+							for (MethodModel methodModel : methods) {
+								System.out.println("\t\t\tmethod: " + methodModel.getName());
+							}
+						}
+					}
+
+					System.out.println("___________");
+
+					// XML serialization
+					String xmlPath = "testFiles/XMLFiles/TestSerialization.xml";
+					File xmlFile = new File(xmlPath);
+					xmlFile.delete(); // deleting the previous instance
+					xmlFile.createNewFile();
+					FileOutputStream fos = new FileOutputStream(xmlFile);
+					ModelFactory projectFactory = new ModelFactory(ProjectModel.class);
+					projectFactory.serialize(projectModel, fos, SerializationPolicy.EXTENSIVE, true);
+					fos.close();
+
+					// Copying the XML file
+					File xmlFileCopy = new File("testFiles/XMLFiles/TestSerializationCopy.xml");
+					FileUtils.copyFile(xmlFile, xmlFileCopy);
+
+					// XML deserialization (of the copy) to obtain a model
+					FileInputStream fis = new FileInputStream(xmlFileCopy);
+					ProjectModel projectModelClone = (ProjectModel) projectFactory.deserialize(fis, DeserializationPolicy.RESTRICTIVE);
+					xmlFileCopy.delete(); // we can delete the copy now
+					fis.close();
+
+					// XML reserialization of the model
+					String xmlPath2 = "testFiles/XMLFiles/TestReserialization.xml";
+					File xmlFile2 = new File(xmlPath2);
+					xmlFile2.delete(); // deleting the previous instance
+					xmlFile2.createNewFile();
+					FileOutputStream fos2 = new FileOutputStream(xmlFile2);
+					projectFactory.serialize(projectModelClone, fos2, SerializationPolicy.EXTENSIVE, true);
+					fos2.close();
+
+					/* Testing all updateModel() methods */
+
+					projectModel.setIsWatching(true);
+
+					// We first need to reference the PackageLink, ClassLink, ClassModel, and AttributeLink we'll be working with
+
+					PackageLink packageLinkTarget = null;
+					List<PackageLink> packageLinks = projectLink.getPackageLinks();
+					for (PackageLink packageLink : packageLinks) {
+						if (packageLink.getPackageModel().getName().equals("firstPackage")) {
+							packageLinkTarget = packageLink;
+							break;
+						}
+					}
+
+					ClassLink classLinkTarget = null;
+					ClassModel classModelTarget = null;
+					List<ClassLink> classLinks = packageLinkTarget.getClassLinks();
+					for (ClassLink classLink : classLinks) {
+						if (classLink.getClassModel().getName().equals("HelloWorld")) {
+							classLinkTarget = classLink;
+							classModelTarget = classLink.getClassModel();
+							break;
+						}
+					}
+
+					AttributeLink attributeLinkTarget = null;
+					List<AttributeLink> attributeLinks = classLinkTarget.getAttributeLinks();
+					for (AttributeLink attributeLink : attributeLinks) {
+						String attributeName = attributeLink.getAttributeModel().getName();
+						if (attributeName.equals("attributeDefault")) {
+							attributeLinkTarget = attributeLink;
+							break;
+						}
+					}
+
+					// For ProjectLink
+
+					showClassModel(classModelTarget); // Showing the current model
+					editFileTest(); // Modifying the name of a class and an attribute
+					System.out.println("Updating the attributeModel via the parent ProjectLink...");
+					projectLink.updateModel();
+					showClassModel(classModelTarget); // Showing that the model has changed
+
+					Thread.sleep(1000); // to give time to the other thread to run and detect the changes
+
+					// For other classes
+
+					editFileTest();
+					System.out.println("Updating the attributeModel via the parent PackageLink...");
+					packageLinkTarget.updateModel();
+					showClassModel(classModelTarget);
+
+					Thread.sleep(1000);
+
+					editFileTest();
+					System.out.println("Updating the attributeModel via the parent ClassLink...");
+					classLinkTarget.updateModel();
+					showClassModel(classModelTarget);
+
+					Thread.sleep(1000);
+
+					// Note: calling attributeLinkTarget.updateModel(); would result in unexpected behavior
+					// The underlying reason for this is that, as of now, an attribute is uniquely defined by its name
+					// So while it's possible to update a classModel by recreating a model from scratch using its path and updating
+					// everything
+					// that has
+					// changed
+					// The same cannot be said of an attributeModel - if we change its name, any attempt at updating it will fail -
+					// From the constructor's perspective, it's as if it was a completely different attribute
+					// So, in fact, it doesn't make sense to call updateModel on attributeLink if the thing we want to update is the
+					// attributeModel
+
+					// TODO Test attributeLinkTarget.updateModel() and methodLinkTarget.updateModel() (with well thought-out tests)
+
+					// Testing file writes
+
+					System.out.println("\nUsing setNameInFile to edit the file...");
+					attributeLinkTarget.setNameInFile("attributeDefault");
+					System.out.println("Updating the model...");
+					classLinkTarget.updateModel();
+					showClassModel(classModelTarget);
+
+					Thread.sleep(1000);
+
+					System.out.println("\nUsing setTypeInFile to edit the file...");
+					attributeLinkTarget.setTypeInFile("int");
+					System.out.println("Updating the model...");
+					classLinkTarget.updateModel();
+					showClassModel(classModelTarget);
+					attributeLinkTarget.setTypeInFile("long"); // Reverting the change
+
+					Thread.sleep(1000);
+
+					// TODO vérification cohérence : classe publique = nom fichier, nom dossier = déclaration package, etc. implique
+					// création de
+					// nouveaux attributs, @Override + changement de nom
+
+					System.exit(0); // Terminating all threads
+
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
 
-				List<MethodModel> methods = classModel.getMethods();
-				for (MethodModel methodModel : methods) {
-					System.out.println("\t\t\tmethod: " + methodModel.getName());
-				}
 			}
-		}
-
-		System.out.println("___________");
-
-		// XML serialization
-		String xmlPath = "testFiles/XMLFiles/TestSerialization.xml";
-		File xmlFile = new File(xmlPath);
-		xmlFile.delete(); // deleting the previous instance
-		xmlFile.createNewFile();
-		FileOutputStream fos = new FileOutputStream(xmlFile);
-		ModelFactory projectFactory = new ModelFactory(ProjectModel.class);
-		projectFactory.serialize(projectModel, fos, SerializationPolicy.EXTENSIVE, true);
-		fos.close();
-
-		// Copying the XML file
-		File xmlFileCopy = new File("testFiles/XMLFiles/TestSerializationCopy.xml");
-		FileUtils.copyFile(xmlFile, xmlFileCopy);
-
-		// XML deserialization (of the copy) to obtain a model
-		FileInputStream fis = new FileInputStream(xmlFileCopy);
-		ProjectModel projectModelClone = (ProjectModel) projectFactory.deserialize(fis, DeserializationPolicy.RESTRICTIVE);
-		xmlFileCopy.delete(); // we can delete the copy now
-		fis.close();
-
-		// XML reserialization of the model
-		String xmlPath2 = "testFiles/XMLFiles/TestReserialization.xml";
-		File xmlFile2 = new File(xmlPath2);
-		xmlFile2.delete(); // deleting the previous instance
-		xmlFile2.createNewFile();
-		FileOutputStream fos2 = new FileOutputStream(xmlFile2);
-		projectFactory.serialize(projectModelClone, fos2, SerializationPolicy.EXTENSIVE, true);
-		fos2.close();
-
-		/*
-		regarder les méthodes de java modernes plutôt qu'openflexo
-		par exemple : java nio
-		directory watcher
-		
-		PamelaResourceFactory resourceFactory = new PamelaResourceFactory();
-		FileSystemBasedResourceCenter resourceCenter = new PamelaResourceModelFactory(FileSystemBasedResourceCenter.class);
-		;
-		
-		File folder = new File(folderPath);
-		
-		resourceCenter.fileModified(folder); // Called when a {@link File} has been modified in directory representing this ResourceCenter
-		resourceCenter.fileAdded(folder); // When one has been discovered
-		resourceCenter.fileDeleted(folder); // When one has been deleted
-		
-		*/
-
-		/* Testing all updateModel() methods */
-
-		projectModel.setIsWatching(true);
-
-		// We first need to reference the PackageLink, ClassLink, ClassModel, and AttributeLink we'll be working with
-
-		PackageLink packageLinkTarget = null;
-		List<PackageLink> packageLinks = projectLink.getPackageLinks();
-		for (PackageLink packageLink : packageLinks) {
-			if (packageLink.getPackageModel().getName().equals("firstPackage")) {
-				packageLinkTarget = packageLink;
-				break;
-			}
-		}
-
-		ClassLink classLinkTarget = null;
-		ClassModel classModelTarget = null;
-		List<ClassLink> classLinks = packageLinkTarget.getClassLinks();
-		for (ClassLink classLink : classLinks) {
-			if (classLink.getClassModel().getName().equals("HelloWorld")) {
-				classLinkTarget = classLink;
-				classModelTarget = classLink.getClassModel();
-				break;
-			}
-		}
-
-		AttributeLink attributeLinkTarget = null;
-		List<AttributeLink> attributeLinks = classLinkTarget.getAttributeLinks();
-		for (AttributeLink attributeLink : attributeLinks) {
-			String attributeName = attributeLink.getAttributeModel().getName();
-			if (attributeName.equals("attributeDefault")) {
-				attributeLinkTarget = attributeLink;
-				break;
-			}
-		}
-
-		// For ProjectLink
-
-		showClassModel(classModelTarget); // Showing the current model
-		editFileTest(); // Modifying the name of a class and an attribute
-		System.out.println("Updating the attributeModel via the parent ProjectLink...");
-		projectLink.updateModel();
-		showClassModel(classModelTarget); // Showing that the model has changed
-
-		// For other classes
-
-		editFileTest();
-		System.out.println("Updating the attributeModel via the parent PackageLink...");
-		packageLinkTarget.updateModel();
-		showClassModel(classModelTarget);
-
-		editFileTest();
-		System.out.println("Updating the attributeModel via the parent ClassLink...");
-		classLinkTarget.updateModel();
-		showClassModel(classModelTarget);
-
-		// Note: calling attributeLinkTarget.updateModel(); would result in unexpected behavior
-		// The underlying reason for this is that, as of now, an attribute is uniquely defined by its name
-		// So while it's possible to update a classModel by recreating a model from scratch using its path and updating
-		// everything
-		// that has
-		// changed
-		// The same cannot be said of an attributeModel - if we change its name, any attempt at updating it will fail -
-		// From the constructor's perspective, it's as if it was a completely different attribute
-		// So, in fact, it doesn't make sense to call updateModel on attributeLink if the thing we want to update is the
-		// attributeModel
-
-		// TODO Test attributeLinkTarget.updateModel() and methodLinkTarget.updateModel() (with well thought-out tests)
-
-		// Testing file writes
-
-		System.out.println("\nUsing setNameInFile to edit the file...");
-		attributeLinkTarget.setNameInFile("attributeDefault");
-		System.out.println("Updating the model...");
-		classLinkTarget.updateModel();
-		showClassModel(classModelTarget);
-
-		System.out.println("\nUsing setTypeInFile to edit the file...");
-		attributeLinkTarget.setTypeInFile("int");
-		System.out.println("Updating the model...");
-		classLinkTarget.updateModel();
-		showClassModel(classModelTarget);
-		attributeLinkTarget.setTypeInFile("long"); // Reverting the change
-
-		// Detecting changes on the disk
-		// TODO
-
-		// TODO vérification cohérence : classe publique = nom fichier, nom dossier = déclaration package, etc. implique
-		// création de
-		// nouveaux attributs, @Override + changement de nom
+		}).start();
 
 	}
 }
